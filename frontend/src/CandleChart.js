808fd8119672158ceaacc956b3bbc668ad9c5937
Jasper Stam <jasper@codeyellow.nl>
2018-05-14 22:08:40 +0200

    Add crosshairs with label

--- frontend/src/CandleChart.js
+++ frontend/src/CandleChart.js
@@ -3,16 +3,29 @@ import PropTypes from 'prop-types';
 
 import { scaleTime } from 'd3-scale';
 import { utcDay } from 'd3-time';
+import { format } from 'd3-format';
+import { timeFormat } from 'd3-time-format';
 
 import { ChartCanvas, Chart } from 'react-stockcharts';
+import {
+    CrossHairCursor,
+    MouseCoordinateX,
+    MouseCoordinateY,
+} from 'react-stockcharts/lib/coordinates';
 import { CandlestickSeries } from 'react-stockcharts/lib/series';
 import { XAxis, YAxis } from 'react-stockcharts/lib/axes';
 import { fitWidth } from 'react-stockcharts/lib/helper';
 import { last, timeIntervalBarWidth } from 'react-stockcharts/lib/utils';
 
-class CandleStickChart extends React.Component {
+@fitWidth
+export default class CandleStickChart extends React.Component {
+    static propTypes = {
+        data: PropTypes.array.isRequired,
+        width: PropTypes.number.isRequired,
+        ratio: PropTypes.number.isRequired,
+    };
     render() {
-        const { type, width, data, ratio } = this.props;
+        const { width, data, ratio } = this.props;
         const xAccessor = d => d.date;
         const xExtents = [xAccessor(last(data)), xAccessor(data[0])];
         return (
@@ -21,7 +34,7 @@ class CandleStickChart extends React.Component {
                 ratio={ratio}
                 width={width}
                 margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
-                type={type}
+                type="svg"
                 seriesName="MSFT"
                 data={data}
                 xAccessor={xAccessor}
@@ -31,23 +44,20 @@ class CandleStickChart extends React.Component {
                 <Chart id={1} yExtents={d => [6000, 11000]}>
                     <XAxis axisAt="bottom" orient="bottom" ticks={6} />
                     <YAxis axisAt="left" orient="left" ticks={5} />
+                    <MouseCoordinateX
+                        at="bottom"
+                        orient="bottom"
+                        displayFormat={timeFormat('%Y-%m-%d')}
+                    />
+                    <MouseCoordinateY
+                        at="left"
+                        orient="left"
+                        displayFormat={format('.0f')}
+                    />
                     <CandlestickSeries width={timeIntervalBarWidth(utcDay)} />
                 </Chart>
+                <CrossHairCursor />
             </ChartCanvas>
         );
     }
 }
-
-CandleStickChart.propTypes = {
-    data: PropTypes.array.isRequired,
-    width: PropTypes.number.isRequired,
-    ratio: PropTypes.number.isRequired,
-    type: PropTypes.oneOf(['svg', 'hybrid']).isRequired,
-};
-
-CandleStickChart.defaultProps = {
-    type: 'svg',
-};
-CandleStickChart = fitWidth(CandleStickChart);
-
-export default CandleStickChart;
