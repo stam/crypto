6aa89fcfc12d5b1a6404287d857e629ffdfc4d15
Jasper Stam <jasper@codeyellow.nl>
2018-05-14 21:52:08 +0200

    Add working stock chart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+import CandleChart from './CandleChart';
 import './App.css';
 
+function parseData(data) {
+    return data.candles.map(candle => ({
+        id: candle.id,
+        open: candle.open / 100,
+        close: candle.close / 100,
+        low: candle.low / 100,
+        high: candle.high / 100,
+        date: new Date(candle.datetime),
+    }));
+}
+
+@observer
 class App extends Component {
+    @observable.ref data;
+
     componentWillMount() {
         fetch('/api', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ query: '{ candles { id open close low high } }' }),
-        }).then(res => res.json()).then(res => {console.log('res', res)});
+            body: JSON.stringify({
+                query: '{ candles { id open close low high datetime} }',
+            }),
+        })
+            .then(res => res.json())
+            .then(res => {
+                this.data = parseData(res.data);
+            });
     }
     render() {
         return (
-            <div className="App">
-                Fetching data
+            <div>
+                {!this.data && <p>Loading</p>}
+                {this.data && <CandleChart data={this.data} />}
             </div>
         );
     }
---
+++ frontend/src/CandleChart.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { scaleTime } from 'd3-scale';
+import { utcDay } from 'd3-time';
+
+import { ChartCanvas, Chart } from 'react-stockcharts';
+import { CandlestickSeries } from 'react-stockcharts/lib/series';
+import { XAxis, YAxis } from 'react-stockcharts/lib/axes';
+import { fitWidth } from 'react-stockcharts/lib/helper';
+import { last, timeIntervalBarWidth } from 'react-stockcharts/lib/utils';
+
+class CandleStickChart extends React.Component {
+    render() {
+        const { type, width, data, ratio } = this.props;
+        const xAccessor = d => d.date;
+        const xExtents = [xAccessor(last(data)), xAccessor(data[0])];
+        return (
+            <ChartCanvas
+                height={400}
+                ratio={ratio}
+                width={width}
+                margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
+                type={type}
+                seriesName="MSFT"
+                data={data}
+                xAccessor={xAccessor}
+                xScale={scaleTime()}
+                xExtents={xExtents}
+            >
+                <Chart id={1} yExtents={d => [6000, 11000]}>
+                    <XAxis axisAt="bottom" orient="bottom" ticks={6} />
+                    <YAxis axisAt="left" orient="left" ticks={5} />
+                    <CandlestickSeries width={timeIntervalBarWidth(utcDay)} />
+                </Chart>
+            </ChartCanvas>
+        );
+    }
+}
+
+CandleStickChart.propTypes = {
+    data: PropTypes.array.isRequired,
+    width: PropTypes.number.isRequired,
+    ratio: PropTypes.number.isRequired,
+    type: PropTypes.oneOf(['svg', 'hybrid']).isRequired,
+};
+
+CandleStickChart.defaultProps = {
+    type: 'svg',
+};
+CandleStickChart = fitWidth(CandleStickChart);
+
+export default CandleStickChart;
