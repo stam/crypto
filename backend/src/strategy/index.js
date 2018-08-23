19790791c6a7e0535f1008fadcbf14be64719a8b
Jasper Stam <jasper.stam@philips.com>
2018-08-23 23:52:30 +0200

    Refactor strategy to use Assets which have individual tactics

--- backend/src/strategy/index.js
+++ backend/src/strategy/index.js
@@ -1,8 +1,33 @@
+const _ = require('lodash');
+
+class Asset {
+  constructor(buyTick, quantity) {
+    this.costBasis = buyTick;
+    this.quantity = quantity;
+    console.log('create Asset', buyTick.get('id'), quantity);
+  }
+
+  handleTick(tick, value) {
+    if (value >= 9500) {
+      this.handleSell(tick);
+    }
+  }
+
+  handleSell() {
+    // should be implemented by its strategy
+    // for now we can only sell the asset in its entirety
+  }
+}
+
 // Dummy strategy, buys at 7000, sells at 9500
 // Without state: doesn't check how much fund is available or active orders
 class Strategy {
   constructor() {
-    this.activeOrder = null;
+    // To be refactored to currency
+    // It currently represents the amount of assets we can buy
+    this.quantity = 1;
+
+    this.assets = [];
     this.orders = [];
   }
 
@@ -11,15 +36,14 @@ class Strategy {
     if (value <= 7000) {
       this.signalBuy(tick);
     }
-    if (value >= 9500) {
-      this.signalSell(tick);
-    }
+
+    _.each(this.assets, asset => asset.handleTick(tick, value));
   }
 
   // Buy if we have no active order
   signalBuy(tick) {
-    if (this.activeOrder === null) {
-      this.activeOrder = this.createOrder(tick, 'buy');
+    if (this.quantity > 0) {
+      this.activeOrder = this.buyAsset(tick, this.quantity);
     }
   }
 
@@ -31,12 +55,42 @@ class Strategy {
     }
   }
 
-  createOrder(tick, type) {
+  //
+  buyAsset(tick, quantity) {
+    this.quantity -= quantity;
+
+    this.createAsset(tick, quantity);
+    this.createOrder(tick, 'buy', quantity);
+  }
+
+  createAsset(buyTick, quantity) {
+    const asset = new Asset(buyTick, quantity);
+
+    // Make sure the asset sell is handled in the strategy
+    // to fix orders and asset management
+    asset.handleSell = (sellTick) => {
+      this.handleAssetSell(asset, sellTick);
+    }
+    this.assets.push(asset);
+  }
+
+  handleAssetSell(asset, sellTick) {
+    // Remove asset from this.assets
+    const index = this.assets.indexOf(asset);
+    if (index > -1) {
+      this.assets.splice(index, 1);
+    }
+
+    this.createOrder(sellTick, 'sell', asset.quantity);
+  }
+
+  createOrder(tick, type, quantity) {
+    console.log(`> Creating ${type} order: quantity ${quantity}, price: ${tick.get('last')}`)
     const order = {
       type,
       timestamp: tick.get('timestamp'),
       price: tick.get('last'),
-      quantity: 100,
+      quantity,
     };
     this.orders.push(order);
     return order;
