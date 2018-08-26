979c8d67d91ca80c64720528a43a867cc83fd0af
Jasper Stam <jasper.stam@philips.com>
2018-08-26 13:32:40 +0200

    Make database connection configurable

---
+++ backend/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: 'postgres',
+    password: null,
+    database: 'postgres',
+    host: 'localhost',
+    port: 5434,
+    dialect: 'postgres'
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  }
+}
---
+++ backend/src/strategy/forking.js
@@ -0,0 +1,23 @@
+const BaseStrategy = require('.');
+const Asset = require('./asset');
+
+class HodlAsset extends Asset {
+  determineSell(value) {
+    console.log('value', value, this.cost);
+    // Sell if we made 10% profit...
+    return value / this.cost >= 1.1;
+  }
+}
+
+class ForkingStrategy extends BaseStrategy {
+  get Asset() {
+    return HodlAsset;
+  }
+  // In this strategy we just hodl anyway
+  // In the future we want to have some kind of enter strat
+  determineBuy() {
+    return true;
+  }
+}
+
+module.exports = ForkingStrategy;
