4cff1133823fd9b579a54889fcd0bcba9fa6b85e
Jasper Stam <jasper.stam@philips.com>
2019-05-31 17:18:21 +0200

    Remove return outside a function

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -10,24 +10,24 @@ if (process.env.NODE_ENV === 'test') {
     migrations: ['migrations/**/*.ts'],
     subscribers: ['src/subscribers/**/*.ts'],
   };
-  return;
+} else {
+  module.exports = {
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_POST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: false,
+    logging: false,
+    entities: ['src/models/**/*.ts'],
+    migrations: ['migrations/**/*.ts'],
+    subscribers: ['src/subscribers/**/*.ts'],
+    cli: {
+      entitiesDir: 'src/models',
+      migrationsDir: 'migrations',
+      subscribersDir: 'src/subscribers',
+    },
 }
 
-module.exports = {
-  type: process.env.DB_TYPE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_POST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  synchronize: false,
-  logging: false,
-  entities: ['src/models/**/*.ts'],
-  migrations: ['migrations/**/*.ts'],
-  subscribers: ['src/subscribers/**/*.ts'],
-  cli: {
-    entitiesDir: 'src/models',
-    migrationsDir: 'migrations',
-    subscribersDir: 'src/subscribers',
-  },
 };
