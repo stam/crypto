b88c454b1267e3b9425ed06c0e37e82ce6e9d275
Jasper Stam <jasper@strnk.nl>
2019-10-10 12:00:02 +0200

    Move OrderAnnotations to parent component to fix zoom bug

--- frontend/src/component/CandleChart.js
+++ frontend/src/component/CandleChart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { zipObject } from 'lodash';
 import { scaleTime } from 'd3-scale';
 import { utcDay } from 'd3-time';
 import { format } from 'd3-format';
@@ -13,6 +13,7 @@ import {
   CurrentCoordinate,
   MouseCoordinateY,
 } from 'react-stockcharts/lib/coordinates';
+import { Annotate, SvgPathAnnotation, buyPath, sellPath } from 'react-stockcharts/lib/annotation';
 import { ema } from 'react-stockcharts/lib/indicator';
 import { CandlestickSeries, LineSeries } from 'react-stockcharts/lib/series';
 import { MovingAverageTooltip } from 'react-stockcharts/lib/tooltip';
@@ -21,7 +22,20 @@ import { fitDimensions } from 'react-stockcharts/lib/helper';
 import { last, timeIntervalBarWidth } from 'react-stockcharts/lib/utils';
 
 import { observer } from 'mobx-react';
-import OrderAnnotations from './OrderAnnotations';
+
+const toDateString = d => d.date.toISOString().substring(0, 10);
+
+const sellProps = {
+  fill: '#FF0000',
+  path: sellPath,
+  tooltip: 'Sell',
+};
+
+const buyProps = {
+  fill: '#006517',
+  path: buyPath,
+  tooltip: 'Buy',
+};
 
 class CandleStickChart extends React.Component {
   static propTypes = {
@@ -32,8 +46,27 @@ class CandleStickChart extends React.Component {
     simulation: PropTypes.object.isRequired,
   };
 
+  filterCandleOnOrderDate(orders) {
+    return candle => {
+      const orderDates = orders.map(o => o.date.substring(0, 10));
+      return orderDates.includes(toDateString(candle));
+    };
+  }
+
+  generateOrderPriceSelector(orders) {
+    const orderDates = orders.map(o => o.date.substring(0, 10));
+    const mappedOrders = zipObject(orderDates, orders);
+
+    return ({ yScale, datum }) => {
+      const date = toDateString(datum);
+      const order = mappedOrders[date];
+      return yScale(parseInt(order.resultPrice / 100, 10));
+    };
+  }
+
   render() {
     const { width, height, data, ratio, simulation } = this.props;
+    const orders = simulation.orders || [];
     const xAccessor = d => d.date;
     const xExtents = [xAccessor(last(data)), xAccessor(data[0])];
 
@@ -55,6 +88,18 @@ class CandleStickChart extends React.Component {
       })
       .accessor(d => d.emaB);
 
+    const buyOrders = orders.filter(o => o.side === 'buy');
+    const sellOrders = orders.filter(o => o.side === 'sell');
+
+    const enhancedBuyProps = {
+      ...buyProps,
+      y: this.generateOrderPriceSelector(buyOrders),
+    };
+    const enhancedSellProps = {
+      ...sellProps,
+      y: this.generateOrderPriceSelector(sellOrders),
+    };
+
     const calculatedData = emaA(emaB(data));
 
     return (
@@ -74,7 +119,20 @@ class CandleStickChart extends React.Component {
           <XAxis axisAt="bottom" orient="bottom" ticks={6} />
           <YAxis axisAt="left" orient="left" ticks={5} />
           <MouseCoordinateX at="bottom" orient="bottom" displayFormat={timeFormat('%Y-%m-%d')} />
-          {simulation.orders && <OrderAnnotations orders={simulation.orders} />}
+          {simulation.orders && (
+            <React.Fragment>
+              <Annotate
+                with={SvgPathAnnotation}
+                when={this.filterCandleOnOrderDate(buyOrders)}
+                usingProps={enhancedBuyProps}
+              />
+              <Annotate
+                with={SvgPathAnnotation}
+                when={this.filterCandleOnOrderDate(sellOrders)}
+                usingProps={enhancedSellProps}
+              />
+            </React.Fragment>
+          )}
           <MouseCoordinateY at="left" orient="left" displayFormat={format('.0f')} />
           <LineSeries yAccessor={emaA.accessor()} stroke={emaA.stroke()} />
           <LineSeries yAccessor={emaB.accessor()} stroke={emaB.stroke()} />
